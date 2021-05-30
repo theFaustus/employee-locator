ee969319e4aa2c08ffc3f16cf8d83b60bf9e1aa7
ipascari <jhoonnyc@gmail.com>
2021-05-30 21:47:22 +0300

    add the project skeleton

---
+++ src/main/java/com/evil/inc/employeelocator/EmployeeLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.evil.inc.employeelocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evil/inc/employeelocator/EmployeeLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.inc.employeelocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
